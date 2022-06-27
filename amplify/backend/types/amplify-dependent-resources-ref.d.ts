fbf73ce23477de814264e1a22c944f79b65836a3
abraralvinirab <abrar15-9800@diu.edu.bd>
2022-06-27 23:01:37 +0600

    added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "predictions": {
+        "audio": {
+            "region": "string",
+            "language": "string"
+        }
     }
 }
